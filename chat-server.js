19dc11697751d194d6a01a830554fa479a13c508
Kenneth McNelly <kmcnelly@wustl.edu>
2018-11-09 02:03:08 -0600

    trying to get username working

--- chat-server.js
+++ chat-server.js
@@ -3,6 +3,9 @@ var http = require("http"),
 	socketio = require("socket.io"),
 	fs = require("fs");
 
+var usernames = [];
+
+
 // Listen for HTTP connections.  This is essentially a miniature static file server that only serves our one file, client.html:
 var app = http.createServer(function(req, resp){
 	// This callback runs when a new connection is made to our HTTP server.
@@ -21,7 +24,25 @@ app.listen(3456);
 var io = socketio.listen(app);
 io.sockets.on("connection", function(socket){
 	// This callback runs when a new Socket.IO connection is established.
-	
+
+	//new user entered 
+	socket.on('new_user', function(data, callback){
+		if(usernames.indexOf(data) != -1){
+			callback(false);
+
+			console.log("Username same");
+		}
+		else{
+			callback(true);
+			socket.username = data;
+			nicknames.push(socket.username);
+			io.sockets.emit('usernames', usernames);
+
+			console.log("Username registered");
+		}
+		
+	}
+
 	socket.on('message_to_server', function(data) {
 		// This callback runs when the server receives a new message from the client.
 		
