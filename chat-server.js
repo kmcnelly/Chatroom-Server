9e1adab63896bd5c05fe20c302b31e3f716dedf8
alex <alexchow700@gmail.com>
2018-11-09 14:43:34 -0600

    trying to hopefully revert to kenneth's version

--- chat-server.js
+++ chat-server.js
@@ -3,11 +3,8 @@ var http = require("http"),
 	socketio = require("socket.io"),
 	fs = require("fs");
 
-<<<<<<< HEAD
-=======
 var usernames = [];
 
->>>>>>> 19dc11697751d194d6a01a830554fa479a13c508
 
 // Listen for HTTP connections.  This is essentially a miniature static file server that only serves our one file, client.html:
 var app = http.createServer(function(req, resp){
@@ -27,22 +24,6 @@ app.listen(3456);
 var io = socketio.listen(app);
 
 io.sockets.on("connection", function(socket){
-
-
-	app.get('/', function(req, res) {
-	res.sendfile('index.html');
- });
- 
- var roomno = 1;
- io.on('connection', function(socket) {
-	
-	//Increase roomno 2 clients are present in a room.
-	if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms["room-"+roomno].length > 1) roomno++;
-	socket.join("room-"+roomno);
- 
-	//Send this event to everyone in the room.
-	io.sockets.in("room-"+roomno).emit('connectToRoom', "You are in room no. "+roomno);
- })
  
 	// This callback runs when a new Socket.IO connection is established.
 
