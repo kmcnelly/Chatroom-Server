62b0ef452d84d9dd7fa5e9469c2685edcbec537f
Kenneth McNelly <kmcnelly@wustl.edu>
2018-11-11 17:27:54 -0600

    working USERS!

--- chat-server.js
+++ chat-server.js
@@ -25,32 +25,50 @@ var io = socketio.listen(app);
 io.sockets.on("connection", function(socket){
 	// This callback runs when a new Socket.IO connection is established.
 
+	//emits updated user list
+	function updateUsers(){
+		io.sockets.emit('getUsers', usernames);
+	}
+
 	//new user entered 
 	socket.on('new_user', function(data, callback){
+		// Check if username is already present
 		if(usernames.indexOf(data) != -1){
 			callback(false);
 
 			console.log("Username same");
 		}
 		else{
-
 			console.log("Username registered");
-			
+
 			callback(true);
 			socket.username = data;
-			nicknames.push(socket.username);
-			io.sockets.emit('usernames', usernames);
+			usernames.push(socket.username);
 
-			
+			//emits updated user list
+			updateUsers();
+
 		}
 		
-	}
+	});
 
 	socket.on('message_to_server', function(data) {
 		// This callback runs when the server receives a new message from the client.
 		
 		console.log("message: "+data["message"]); // log it to the Node.JS output
 
-		io.sockets.emit("message_to_client",{message:data["message"] }) // broadcast the message to other users
+		io.sockets.emit("message_to_client", {message:data["message"], user: socket.username }) // broadcast the message to other users
 	});
+
+	socket.on('disconnect', function(data){
+		//if user did not enter the actual chat
+		if(!socket.username){
+			return;
+		}
+
+		//finds and removes only the username from the array – Found Online 
+		usernames.splice(usernames.indexOf(socket.username), 1);
+
+		updateUsers();
+	})
 });
